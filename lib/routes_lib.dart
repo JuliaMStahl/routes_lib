8c8dd7d4ce2c03e136cd0405ff4f97cd91bea8cd
Julia Stahl <julia.stahl@kepha.com.br>
2023-01-27 16:37:49 -0300

    Importa módulo acesso via git

--- lib/routes_lib.dart
+++ lib/routes_lib.dart
@@ -3,6 +3,7 @@ library routes_lib;
 import 'package:get/get.dart';
 import 'package:perfil/app/routes/app_pages.dart' as perfil_routes;
 import 'package:dashboard/app/routes/app_pages.dart' as dashboard_routes;
+import 'package:acesso/app/routes/app_pages.dart' as acesso_routes;
 
 class AppPages {
   AppPages._();
@@ -11,6 +12,7 @@ class AppPages {
     List<GetPage> _routesFull = <GetPage>[];
     _routesFull.addAll(perfil_routes.AppPages.routes);
     _routesFull.addAll(dashboard_routes.AppPages.routes);
+    _routesFull.addAll(acesso_routes.AppPages.routes);
     return _routesFull;
   }
 }
@@ -20,4 +22,5 @@ abstract class RedirectTo {
 
   static const ROUTES_PERFIL = perfil_routes.Routes.PERFIL_PAGE;
   static const ROUTES_DASHBOARD = dashboard_routes.Routes.DASHBOARD_PAGE;
+  static const ROUTES_ACESSO = acesso_routes.Routes.ACESSO_PAGE;
 }
