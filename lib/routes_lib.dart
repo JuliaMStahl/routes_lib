8f62505701f0375389a704b0862bebc97668965c
Julia Stahl <julia.stahl@kepha.com.br>
2023-01-27 15:27:20 -0300

    Importa Get e cria classes AppPages e RedirectTo

--- lib/routes_lib.dart
+++ lib/routes_lib.dart
@@ -1,3 +1,20 @@
 library routes_lib;
 
-export 'package:perfil/app/routes/app_pages.dart';
+import 'package:get/get.dart';
+import 'package:perfil/app/routes/app_pages.dart' as perfil_routes;
+
+class AppPages {
+  AppPages._();
+
+  static List<GetPage> get routes {
+    List<GetPage> _routesFull = <GetPage>[];
+    _routesFull.addAll(perfil_routes.AppPages.routes);
+    return _routesFull;
+  }
+}
+
+abstract class RedirectTo {
+  RedirectTo._();
+
+  static const ROUTES_PERFIL = perfil_routes.Routes.PERFIL_PAGE;
+}
